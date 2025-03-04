ebca36356ba26fcdf44b1a0a85aa3b7ea02d11dc
theo <thjmaurino@gmail.com>
2025-03-03 21:58:52 -0600

    recipe rendering looking a bit better. still a lot of work to do but this is just prelim

--- src/components/Recipe/RenderRecipeMetadata.tsx
+++ src/components/Recipe/RenderRecipeMetadata.tsx
@@ -6,24 +6,24 @@ type Props = {
 
 export const RenderRecipeMetadata = ({ meta }: Props) => {
     return (
-        <div className="recipe-metadata flex my-2">
+        <div className="recipe-metadata flex my-4 gap-4">
             {meta.yield && (
-                <div className="metadata-yield rounded-full px-2 py-1 bg-gray-100">
+                <div className="metadata-yield rounded-full px-4 shadow-inner bg-gray-100">
                     <p>Yield : {meta.yield}</p>
                 </div>
             )}
             {meta.prepTime && (
-                <div className="metadata-yield rounded-full px-2 py-1 bg-gray-100">
+                <div className="metadata-yield rounded-full px-4 shadow-inner bg-gray-100">
                     <p>Preparation Time : {meta.prepTime} minutes</p>
                 </div>
             )}
             {meta.cookTime && (
-                <div className="metadata-yield rounded-full px-2 py-1 bg-gray-100">
+                <div className="metadata-yield rounded-full px-4 shadow-inner bg-gray-100">
                     <p>Cook Time : {meta.cookTime} minutes</p>
                 </div>
             )}
             {meta.totalTime && (
-                <div className="metadata-yield rounded-full px-2 py-1 bg-gray-100">
+                <div className="metadata-yield rounded-full px-4 shadow-inner bg-gray-100">
                     <p>Total : {meta.totalTime} minutes</p>
                 </div>
             )}
