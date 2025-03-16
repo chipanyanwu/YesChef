9ba9debac40feccb986c58e09ca8e1315dfd38ce
brookshu <134887313+brookshu@users.noreply.github.com>
2025-03-15 23:25:41 -0400

    unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,10 +4,17 @@ import App from "./App"
 
 window.HTMLElement.prototype.scrollIntoView = function () {}
 
-describe("does page load", () => {
+describe("does starting screen load", () => {
   test("loading", () => {
     render(<App />)
     expect(screen.getByText("Welcome to YesChef! Paste a recipe page (all ads and pictures included) to get started.")).toBeDefined()
   })
 
+  test("Counter should increment by one when clicked", async () => {
+    render(<App />)
+    const text = screen.getByRole("textbox")///, { name: 'Ask Chef...' })
+    fireEvent.change(text, { target: { value: 'Hello, Chef!' } })
+    expect(await screen.getByText("Hello, Chef!")).toBeDefined()
+  })
 })
+
